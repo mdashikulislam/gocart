242d6993e4df24c9ed2971717832076b4857eaa7
mdashikulislam <ashik.nwu@gmail.com>
2023-12-04 21:57:07 +0600

    lastest changes

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-include_once './vendor/autoload.php';
+//include_once './vendor/autoload.php';
 
 //set the session userdata if non-existant
 if(!isset($_SESSION['userdata']))
