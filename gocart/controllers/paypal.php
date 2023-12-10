b76512be213e6e7757ae205fb539fe9a5b01c525
mdashikulislam <ashik.nwu@gmail.com>
2023-12-11 03:29:47 +0600

    update

--- gocart/controllers/paypal.php
+++ gocart/controllers/paypal.php
@@ -1,5 +1,7 @@
 <?php
-class paypal extends Front_Controller{
+
+class paypal extends Front_Controller
+{
 
     public function __construct()
     {
@@ -8,9 +10,94 @@ class paypal extends Front_Controller{
 
     public function success()
     {
-        pp('as');
-    }
-    public function cancel(){
+        $token = $this->input->get('token');
+        $payer = $this->input->get('PayerID');
+        $settings = $this->Settings_model->get_settings('paypal_express');
+        $paypalAuth = $settings['username'].':'.$settings['password'];
 
+        try {
+            $curl = curl_init();
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => 'https://api.sandbox.paypal.com/v2/checkout/orders/'.$token.'/capture',
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => '',
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 0,
+                CURLOPT_FOLLOWLOCATION => true,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_HTTPAUTH=>CURLAUTH_BASIC,
+                CURLOPT_USERPWD=>$paypalAuth,
+                CURLOPT_CUSTOMREQUEST => 'POST',
+                CURLOPT_POSTFIELDS => '{"payer_id":"'.$payer.'"}',
+                CURLOPT_HTTPHEADER => array(
+                    'Content-Type: application/json',
+                    'Prefer: return=representation',
+                    'PayPal-Request-Id: ',time()
+                ),
+            ));
+            $response = curl_exec($curl);
+            curl_close($curl);
+            $responseData = json_decode($response,true);
+            if ($responseData['status'] == 'COMPLETED'){
+                $this->go_cart->set_payment_confirmed();
+                redirect('checkout/place_order/');
+            }else{
+                $this->session->set_flashdata('message', "<div>paypal did not validate your order. Either it has been processed already, or something else went wrong. If you believe there has been a mistake, please contact us.</div>");
+                redirect('checkout');
+            }
+        }catch (Exception $exception){
+            $this->session->set_flashdata('message', "<div>".$exception->getMessage()."</div>");
+            redirect('checkout');
+        }
+
+    }
+
+    public function cancel()
+    {
+
+    }
+
+    function tokenUpdate()
+    {
+        $settings = $this->Settings_model->get_settings('paypal_express');
+        $clientID = '';
+        $clientSecret = '';
+        if ($settings['SANDBOX'] == '1') {
+            $clientID = $settings['username'];
+            $clientSecret = $settings['password'];
+        } else {
+            $clientID = $settings['username'];
+            $clientSecret = $settings['password'];
+        }
+        $apiEndpoint = 'https://api-m.sandbox.paypal.com/v1/oauth2/token'; // Sandbox URL, change to production URL for live transactions
+        $tokenRequestData = [
+            'grant_type' => 'client_credentials'
+        ];
+
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_URL, $apiEndpoint);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_USERPWD, $clientID . ':' . $clientSecret);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($tokenRequestData));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Accept: application/json',
+            'Accept-Language: en_US'
+        ]);
+
+        $response = curl_exec($ch);
+
+        curl_close($ch);
+        $currentToken = '';
+        $currentTime = time();
+        $responseData = json_decode($response, true);
+        if (!empty($responseData['access_token'])) {
+            $currentToken = $responseData['access_token'];
+            $currentTime = $currentTime + (int)$responseData['expires_in'];
+        }
+        $this->Settings_model->save_settings('paypal_express', [
+            'signature' => $currentToken,
+            'token_expire' => $currentTime
+        ]);
     }
 }
